af287eb26050f00362f45aafca20b10781c2de47
Arash Altafi <arashaltafi1377@gmail.com>
2024-06-04 14:05:24 +0430

    delete font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Anton } from 'next/font/google'
 import { Container } from 'react-bootstrap';
 import Head from 'next/head';
-
-const anton = Anton({ subsets: ['latin'], weight: ["400"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -16,14 +13,11 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <div className={anton.className}>
-        <main>
-          <Container className='py-4'>
-            <Component {...pageProps} />
-          </Container>
-        </main>
-      </div>
+      <main>
+        <Container className='py-4'>
+          <Component {...pageProps} />
+        </Container>
+      </main>
     </>
   );
 }
